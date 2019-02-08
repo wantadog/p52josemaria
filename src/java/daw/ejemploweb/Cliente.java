3d87149b3f8d12573a7e8f75a59752f016fda6e9
wantadog <jlopmar196@gmail.com>
2019-02-08 09:20:12 +0100

    Añade clase cliente

---
+++ src/java/daw/ejemploweb/Cliente.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package daw.ejemploweb;
+
+/**
+ *
+ * @author chemorrow
+ */
+public class Cliente {
+    private String nombre;
+    private int edad;
+    private String email;
+
+    public Cliente(String nombre, int edad, String email) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.email = email;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "nombre=" + nombre + ", edad=" + edad + ", email=" + email + '}';
+    }
+}
